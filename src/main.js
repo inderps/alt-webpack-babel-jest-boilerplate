9d2285fb981612b2add22dd3d3500de9b95ad5b9
Inderpal Singh <inderpal@pfeffermind-games.de>
2015-09-28 09:30:00 +0200

    added .jscsrc file and some minor fixes

--- src/main.js
+++ src/main.js
@@ -1,15 +1,9 @@
 import 'babel/polyfill';
 import React from 'react';
-import { Router, Route, Link } from 'react-router'
+import { Router, Route } from 'react-router';
 import FastClick from 'fastclick';
 import App from './components/App/App';
 const appContainer = document.getElementById('app');
-
-const Routes = (
-  <Route handler={App}>
-  <Route name="/" handler={App}/>
-  </Route>
-);
 
 function run() {
   FastClick.attach(document.body);
